2b9af7778134f39bf0e4a183f34cc13e221805df
yangbin <yangbin@star-net.cn>
2020-05-29 17:37:11 +0800

    FEATURE import/upload 接口调试中

--- test.js
+++ test.js
@@ -1,13 +1,31 @@
+
+const fs = require('fs');
 const SevenConvert = require('./lib/index');
 
 const sevenConvert = new SevenConvert('test');
 
-(async () => {
+/* 新建转换任务 */
+/*(async () => {
     const data = await sevenConvert.tasks.create('convert', {
+        'input': 'input',
         'name': 'test',
         'url': 'http://invalid.url',
-        'filename': 'test.file'
+        'filename': 'test.file',
+        'output_format': '7z'
     });
 
     console.log(data);
+})();*/
+
+/* 新建上传任务 */
+(async () => {
+    const task = await sevenConvert.tasks.create('import/upload');
+
+    console.log(task);
+
+    const stream = fs.createReadStream(__dirname + './tests/integration/files/input.png');
+
+    await sevenConvert.tasks.upload(task, stream);
+
+    console.log('upload success !');
 })();
